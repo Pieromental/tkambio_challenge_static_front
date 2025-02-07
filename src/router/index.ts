a0a95bcdd20a05c27f49370a3d6cc134df66137c
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2025-02-07 13:31:31 -0500

    fix:BASE  URL problems

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { authRoutes } from '@/modules/auth/routes/index'
 import { reportRoutes } from '@/modules/report/routes'
 const router = createRouter({
-  history: createWebHashHistory('#'),
+  history: createWebHistory('/tkambio_challenge_static_front/'),
   routes: [
     {
       path: '/',
