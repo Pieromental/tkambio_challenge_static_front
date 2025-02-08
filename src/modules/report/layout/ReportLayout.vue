fd44b705bc5de13ad810f8080747f56464126436
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2025-02-08 00:54:28 -0500

    feat:Add logout

--- src/modules/report/layout/ReportLayout.vue
+++ src/modules/report/layout/ReportLayout.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="layout-container">
     <div class="header">
-      <img
-        class="layout__left_box-container-logo"
-        src="@/assets/image/bg-tkambio-v2.png"
-        alt="logo tkambio"
-      />
+      <img class="logo-img" src="@/assets/image/bg-tkambio-v2.png" alt="logo tkambio" />
+      <button @click="logout">
+        <img :src="logOut" alt="Icono" class="button-icon" />
+      </button>
     </div>
     <div class="router-view">
       <RouterView />
     </div>
   </div>
 </template>
-
+<script lang="ts" setup>
+/****************************************************************************/
+/*                               IMPORTS                                     */
+/****************************************************************************/
+import { useRouter } from 'vue-router'
+import { useAlert } from '@/composable/alert/useAlert'
+import logOut from '@/assets/icons/log_out.png'
+/****************************************************************************/
+/*                               COMPOSABLE                                  */
+/****************************************************************************/
+const router = useRouter()
+const { showAlert } = useAlert()
+/****************************************************************************/
+/*                               METHODS                                    */
+/****************************************************************************/
+const logout = async () => {
+  const confirm = await showAlert({
+    type: 'warning',
+    mode: 'confirm',
+    title: '',
+    message: '¿Está seguro de querer cerrar sesión?.',
+  })
+  if (confirm) {
+    localStorage.clear()
+    sessionStorage.clear()
+    router.push({ name: 'login' })
+  }
+}
+</script>
 <style lang="sass" scoped>
 @import '@/assets/styles/main.sass'
 .header
@@ -22,13 +49,29 @@
     align-items: center
     justify-content: center
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3)
+    position: relative
     @include responsive("lg", "max")
         padding-left: 20px
         justify-content: left
 
+button
+  position: absolute  /* Usa posición absoluta para colocar el botón a la derecha */
+  right: 20px  /* Ajusta la distancia del borde derecho */
+  background: none
+  border: none
+  height: 60%
+  cursor: pointer
+  border-radius: 5px
+  transition:  box-shadow 0.3s
+  display: inline-block
+  &:hover
+     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1)
 
-img
-    height: 50%
+.logo-img
+  height: 60%
+
+.button-icon
+  height: 80%
 
 .router-view
     height: 88vh
